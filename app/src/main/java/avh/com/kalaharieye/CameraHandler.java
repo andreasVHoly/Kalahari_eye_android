eea58e7c48efe07ba7a1595f71832977f110ee6c
Andreas von Holy <andreas409@gmail.com>
2016-01-26 22:12:27 +0200

    Initial revision

---
+++ app/src/main/java/avh/com/kalaharieye/CameraHandler.java
@@ -0,0 +1,139 @@
+package avh.com.kalaharieye;
+
+/**
+ * Created by SMNM on 26/1/2016.
+ */
+
+import org.opencv.videoio.VideoCapture;
+
+public class CameraHandler {
+
+
+    //"http://admin:1234@192.168.0.101:8080/videostream.asf?user=admin&pwd=1234&resolution=64&rate=0";
+
+    private int startIP, endIP;
+    private String port, userName, password, connectionAddress;
+    private String startString = "http://";
+    private String endString = "&resolution=64&rate=0";
+    private String midString  = "/videostream.asf?user=";
+    private String ipAddress = "@192.168.0.";
+
+    public CameraHandler(int startIP, int endIP, String port, String username, String password){
+        this.port = port;
+        this.startIP = startIP;
+        this.endIP = endIP;
+        this.userName = username;
+        this.password = password;
+    }
+
+
+    public boolean connectToCamera(){
+
+
+        String connectionAddressStart = startString + userName + ":" + password + ipAddress;
+        String connectionAddressEnd = ":" + port + midString + userName + "&pwd=" + password + endString;
+        VideoCapture vcap = new VideoCapture();
+
+        boolean successfullConnect = false;
+
+        for (int i = startIP; i < endIP; i++){
+            connectionAddress = connectionAddressStart + Integer.toString(i) + connectionAddressEnd;
+            if (vcap.open(connectionAddress)){
+                System.out.println("Connected to camera at address " + ipAddress + i);
+                successfullConnect = true;
+                break;
+            }
+            System.out.println("Could not connect to camera at address " + ipAddress + i);
+        }
+        return successfullConnect;
+
+    }
+
+
+
+
+    public void setStartIP(int startIP) {
+        this.startIP = startIP;
+    }
+
+    public int getEndIP() {
+        return endIP;
+    }
+
+    public void setEndIP(int endIP) {
+        this.endIP = endIP;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getConnectionAddress() {
+        return connectionAddress;
+    }
+
+    public void setConnectionAddress(String connectionAddress) {
+        this.connectionAddress = connectionAddress;
+    }
+
+    public int getStartIP() {
+        return startIP;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStartString() {
+        return startString;
+    }
+
+    public void setStartString(String startString) {
+        this.startString = startString;
+    }
+
+    public String getEndString() {
+        return endString;
+    }
+
+    public void setEndString(String endString) {
+        this.endString = endString;
+    }
+
+    public String getMidString() {
+        return midString;
+    }
+
+    public void setMidString(String midString) {
+        this.midString = midString;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/avh/com/kalaharieye/MainActivity.java
@@ -0,0 +1,19 @@
+package avh.com.kalaharieye;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(avh.com.kalaharieye.R.layout.activity_main);
+    }
+
+
+
+
+
+}
